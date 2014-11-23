0ef09a46f18b9c9717d625be75e4c73a38d8008d
Chris Chamberlain <c.chamberlain@iinet.net.au>
2014-11-23 15:45:20 +1100

    Added Authentication support.

---
+++ src/Behaviour/Authentication.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Oxygen\Data\Behaviour;
+
+use Doctrine\ORM\Mapping AS ORM;
+
+trait Authentication {
+
+    use RememberToken;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+
+    private $password;
+
+    /**
+     * Gets the user's password.
+     *
+     * @return mixed
+     */
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    /**
+     * Sets the user's password.
+     *
+     * @param string $password
+     * @return void
+     */
+
+    public function setPassword($password) {
+        $this->password = $password;
+    }
+
+    /**
+     * Get the unique identifier for the user.
+     *
+     * @return mixed
+     */
+
+    public function getAuthIdentifier() {
+        return $this->getId();
+    }
+
+    /**
+     * Get the password for the user.
+     *
+     * @return string
+     */
+
+    public function getAuthPassword() {
+        return $this->getPassword();
+    }
+
+    /**
+     * Retrieves the id field name
+     *
+     * @return string
+     */
+
+    public function getKeyName() {
+        return 'id';
+    }
+
+}
