ba7c440e9f4d31a27771bd4dc7aec59bf280b0ff
Chris Chamberlain <c.chamberlain@iinet.net.au>
2015-12-07 08:46:25 +1100

    Updated to use new config path for laravel doctrine

--- src/BaseServiceProvider.php
+++ src/BaseServiceProvider.php
@@ -7,15 +7,15 @@ use Illuminate\Support\ServiceProvider as ServiceProvider;
 abstract class BaseServiceProvider extends ServiceProvider {
 
     /**
-     * Register a directory of Doctrine entities.
+     * Register a namespace of Doctrine entities.
      *
-     * @param  string  $directory
+     * @param  string  $namespace
      * @return void
      */
-    public function loadEntitiesFrom($directory) {
-        $metadata = $this->app['config']['doctrine.metadata'];
-        $metadata[] = $directory;
-        $this->app['config']->set('doctrine.metadata', $metadata);
+    public function loadEntitiesFrom($namespace) {
+        $metadata = $this->app['config']['doctrine.managers.default.namespaces'];
+        $metadata[] = $namespace;
+        $this->app['config']->set('doctrine.managers.default.namespaces', $metadata);
     }
 
 }
